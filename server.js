c70bfd84a35e456c6867d9c0a951fab6f1ce6112
bilel gharbi <hb.bilel@gmail.com>
2021-07-29 14:18:45 +0100

    add cors package

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middleware = jsonServer.defaults({ noCors: true });
 const port = process.env.PORT || 3000;
+const cors = require("cors");
 
+server.use(cors("*"));
 server.use(middleware);
 server.use(router);
 
